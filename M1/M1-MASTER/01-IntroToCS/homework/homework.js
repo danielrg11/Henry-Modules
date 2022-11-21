a0ad959f90e0299650292a67101917368b3828db
unknown <danielrawlins11@hotmail.com>
2022-11-21 16:31:25 -0500

    homework IntroToCs

--- M1/M1-MASTER/01-IntroToCS/homework/homework.js
+++ M1/M1-MASTER/01-IntroToCS/homework/homework.js
@@ -1,8 +1,32 @@
 'use strict';
 
-function BinarioADecimal(num) {}
+function BinarioADecimal(num) {
+   let array = num.split('').reverse();
+   let swap = 0;
+      for(let i = 0; i < array.length ; i++){
+         swap += array[i] * 2 ** i;
+      }
+      return swap;
+}
 
-function DecimalABinario(num) {}
+function DecimalABinario(num) {
+   let array = [];
+   let residuo = 0;
+   // if ( num === 0 ){
+   //    return '0';
+   // }
+ while(num >= 1){
+   residuo = num  % 2;
+   num /=  2;
+  if(residuo  === 0){
+    array.push('0')
+  }
+  else{
+   array.push('1')
+  }
+ }
+ return array.reverse().join('');
+}
 
 module.exports = {
    BinarioADecimal,
