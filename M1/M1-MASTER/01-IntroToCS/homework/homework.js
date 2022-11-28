3b90c32447c9dfe66b9ec00620249a2316f4a204
unknown <danielrawlins11@hotmail.com>
2022-11-28 16:44:25 -0500

    complete and recursive practice

--- M1/M1-MASTER/01-IntroToCS/homework/homework.js
+++ M1/M1-MASTER/01-IntroToCS/homework/homework.js
@@ -1,6 +1,6 @@
 'use strict';
 
-function BinarioADecimal(num) {
+function BinarioADecimal1(num) {
    let array = num.split('').reverse();
    let swap = 0;
       for(let i = 0; i < array.length ; i++){
@@ -8,13 +8,39 @@ function BinarioADecimal(num) {
       }
       return swap;
 }
+console.log(BinarioADecimal1("11110000110"))
+
+// Binario a decimal con recursividad 
+function BinarioADecimal(str,i){
+ let n = str.length ; // 1101 length = 4 / n=4
+ if (i == n-1){      // if 3 == 3 = true
+   return str[i] -''; // true // 8/4/0/1 = '13'
+ }
+ return ( str[i] << (n-i-1)) + BinarioADecimal(str,i+1); // 8//4//0//1
+} 
+
+
+
+console.log(BinarioADecimal("1101",0))
+
+// function BinarioADecimal(str,i){
+//    let n = str.length;
+//    let resul = 0
+//    if(str.length === 0){
+//       return resul ;
+//    }
+//    return 
+// }
+// console.log(BinarioADecimal('1010',0))
+
+
 
 function DecimalABinario(num) {
    let array = [];
    let residuo = 0;
-   // if ( num === 0 ){
-   //    return '0';
-   // }
+    if ( num === 0 ){
+      return '0';
+    }
  while(num >= 1){
    residuo = num  % 2;
    num /=  2;
@@ -26,6 +52,12 @@ function DecimalABinario(num) {
   }
  }
  return array.reverse().join('');
+}
+// Decimal A Binario con Recursividad
+function DecimalABinario(num) {
+   if (num === 1){return '1';}
+   if (num === 0){return '0';}
+ return DecimalABinario(Math.floor(num / 2)) + (num%2);
 }
 
 module.exports = {
